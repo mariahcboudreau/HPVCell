ebb8e154543ca170475efbd7900a97dff2b2c101
Mariah Boudreau <mcboudre@ip0af5c050.int.uvm.edu>
2023-06-12 16:13:44 -0400

    updates

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def MOM(m, t, beta, gamma, delta, rho, theta): #figure out the right way to do t
     # First Moment for p
     dx[1] = (rho - theta) * m[1] + (2 * delta + gamma) * m[0]
     # Second Moment for b
-    dx[2] = 2*(beta - gamma) * m[2] + (beta + gamma) * m[0]
+    dx[2] = 2*(beta - delta) * m[2] + (beta + delta) * m[0]
     # Second Moment for p
     dx[3] = (theta + rho) * m[1] + (2 * rho - 2 * theta) * m[3] + (2 * gamma + 4 * delta) * m[4] + (gamma + 4 * delta) * m[0]
     # Covariance for bp
@@ -92,7 +92,7 @@ x_0 = np.zeros((nb_of_states_b, nb_of_states_p))
 x_0[1,0] = 1
 m_0 = np.zeros(5)
 m_0[0] = 1
-m_0[2] = 1
+m_0[2] = 0
 
 # Parameters of the model
 symm_div = 0.04
@@ -118,65 +118,72 @@ extinct = np.zeros((500))
 for t in range(499):
     extinct[t] = x_path[t+1][0][0] - x_path[t][0][0]
 
-# plt.plot(t_vec, extinct, label = 'Probability of extinction')
-# plt.legend()
-# plt.ylabel('Probability')
-# plt.xlabel('Time')
-# plt.savefig("extinction_prob.pdf", format = 'pdf')
+plt.plot(t_vec, extinct, label = 'Probability of extinction')
+plt.legend()
+plt.ylabel('Probability')
+plt.xlabel('Time')
+plt.savefig("extinction_prob.pdf", format = 'pdf')
+plt.close()
 
-dist = np.sum(x_path[t], axis=1)
-print(dist)
-print(np.std(dist))
 
-# # # Plot
-# for t in np.arange(1, 499, 100):
-#     plt.plot(range(nb_of_states_b), np.sum(x_path[t], axis=1), marker="o", ls='--',
-#              label=fr"$t = {t_length * t / t_steps:.2f}$")
-# plt.legend()
-# plt.ylabel('Occupation number')
-# plt.xlabel('Number of infected basal cells')
-# plt.savefig("explicit_basal.pdf", format = "pdf")
+
+# # # # Plot
+for t in np.arange(1, 499, 100):
+    plt.plot(range(nb_of_states_b), np.sum(x_path[t], axis=1), marker="o", ls='--',
+             label=fr"$t = {t_length * t / t_steps:.2f}$")
+plt.legend()
+plt.ylabel('Occupation number')
+plt.xlabel('Number of infected basal cells')
+plt.savefig("explicit_basal.pdf", format = "pdf")
+plt.close()
 # #
-# # # Plot
-# for t in np.arange(1,499,100):
-#     plt.plot(range(nb_of_states_p), np.sum(x_path[t], axis=0), marker="o", ls='--', label=fr"$t = {t_length*t/t_steps:.2f}$")
-# plt.legend()
-# plt.ylabel('Occupation number')
-# plt.xlabel('Number of infected parabasal cells')
-# plt.savefig("explicit_parabasal.pdf", format = "pdf")
+# # # # Plot
+for t in np.arange(1,499,100):
+    plt.plot(range(nb_of_states_p), np.sum(x_path[t], axis=0), marker="o", ls='--', label=fr"$t = {t_length*t/t_steps:.2f}$")
+plt.legend()
+plt.ylabel('Occupation number')
+plt.xlabel('Number of infected parabasal cells')
+plt.savefig("explicit_parabasal.pdf", format = "pdf")
+plt.close()
+# # # # Plot
+# # # im = plt.imshow(np.log10(x_path[-1]))
+# # # cbar = plt.colorbar(im)
+# # # plt.ylabel('Number of infected parabasal cells')
+# # # plt.xlabel('Number of infected basal cells')
+# # # cbar.set_label('log10(Occupation)')
+# # # plt.show()
+# #
+stddev = np.sqrt(m_path[-1][2]-m_path[-1][0]**2)
+stddev_parabasal = np.sqrt(m_path[-1][3]-m_path[-1][1]**2)
 
-# # # Plot
-# # im = plt.imshow(np.log10(x_path[-1]))
-# # cbar = plt.colorbar(im)
-# # plt.ylabel('Number of infected parabasal cells')
-# # plt.xlabel('Number of infected basal cells')
-# # cbar.set_label('log10(Occupation)')
-# # plt.show()
-#
-# stddev = np.sqrt(m_path[1][2]-m_path[1][0]**2)
-# stddev_parabasal = np.sqrt(m_path[1][3]-m_path[1][1]**2)
 
-#
-# plt.vlines(m_path[1][0], 0, 0.9, colors='black', linestyles='-', label='First moment')
-# plt.vlines(m_path[1][0]+stddev, 0, 0.9, colors='gray', linestyles='--', label='Standard deviation')
-# plt.vlines(m_path[1][0]-stddev, 0, 0.9, colors='gray', linestyles='--', label='Standard deviation')
-# plt.plot(range(nb_of_states_b), np.sum(x_path[1], axis = 1), marker="o", lw=0, ms=15, label=fr"$t = {t_vec[1]}$")
-# plt.legend()
-# plt.ylabel('Occupation number')
-# plt.xlabel('Number of infected basal cells')
-#
-# plt.savefig("MoM_basal_compare_time1.pdf", format = "pdf")
-#
-# plt.vlines(m_path[1][1], 0, 0.3, colors='black', linestyles='-', label='First moment')
-# plt.vlines(m_path[1][1]+stddev_parabasal, 0, 0.175, colors='gray', linestyles='--', label='Standard deviation')
-# plt.vlines(m_path[1][1]-stddev_parabasal, 0, 0.325, colors='gray', linestyles='--', label='Standard deviation')
-# plt.plot(range(nb_of_states_p), np.sum(x_path[1], axis=0), marker="o", lw=0, ms=15, label=fr"$t = {t_vec[1]}$")
-# plt.legend()
-# plt.ylabel('Occupation number')
-# plt.xlabel('Number of infected parabasal cells')
 
-#plt.savefig("MoM_parabasal_compare_time1.pdf", format = "pdf")
-#
+
+
+plt.vlines(m_path[1][0], 0, 0.9, colors='black', linestyles='-', label='First moment')
+plt.vlines(m_path[1][0]+stddev, 0, 0.9, colors='gray', linestyles='--', label='Standard deviation')
+#plt.vlines(m_path[1][0]-stddev, 0, 0.9, colors='gray', linestyles='--', label='Standard deviation')
+plt.plot(range(nb_of_states_b), np.sum(x_path[1], axis = 1), marker="o", lw=0, ms=15, label=fr"$t = {t_vec[1]}$")
+plt.legend()
+plt.ylabel('Occupation number')
+plt.xlabel('Number of infected basal cells')
+
+
+
+plt.savefig("MoM_basal_compare_time1.pdf", format = "pdf")
+plt.close()
+
+plt.vlines(m_path[1][1], 0, 0.3, colors='black', linestyles='-', label='First moment')
+#plt.vlines(m_path[1][1]+stddev_parabasal, 0, 0.175, colors='gray', linestyles='--', label='Standard deviation')
+#plt.vlines(m_path[1][1]-stddev_parabasal, 0, 0.325, colors='gray', linestyles='--', label='Standard deviation')
+plt.plot(range(nb_of_states_p), np.sum(x_path[1], axis=0), marker="o", lw=0, ms=15, label=fr"$t = {t_vec[1]}$")
+plt.legend()
+plt.ylabel('Occupation number')
+plt.xlabel('Number of infected parabasal cells')
+
+plt.savefig("MoM_parabasal_compare_time1.pdf", format = "pdf")
+plt.close()
+
 #
 #
 #
